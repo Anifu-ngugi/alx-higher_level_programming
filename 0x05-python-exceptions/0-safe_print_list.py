6ee7a6aaf2ad10f6df4be2527195cd9e90b4fb2c
Anifu-ngugi <anifungugi1@gmail.com>
2024-02-04 22:36:00 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    x = 0
     try:
         for i in my_list:
+            print("{}".format(i), end="")
             x += 1
     except NameError:
         print("list was not found")
     else:
         print(x)
-
-# Example usage
-my_list = [1, 2, 3, 4]
-safe_print_list(my_list)
-
