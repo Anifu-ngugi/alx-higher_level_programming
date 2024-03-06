8938c6b6c8a6ec828121cc447b959d68ebcd8a80
Anifu-ngugi <anifungugi1@gmail.com>
2024-03-06 05:58:55 -0800

    working basic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    a = len(sys.argv) - 1
-    if a == 1:
-        print('{} argument.'.format(a), end="\n")
-        print('{} : {}'.format(1, sys.argv[a]), end="\n")
-    elif a >= 1:
-        print('{} arguments:'.format(a), end="\n")
-        for i in range(1, a + 1):
-            print('{}: {}'.format(i, sys.argv[i]), end="\n")
+    arg_len = len(sys.argv) - 1
+    if arg_len == 1:
+        print("{} argument:".format(arg_len), end="\n")
+        print("{}: {}".format(1, sys.argv[arg_len]), end="\n")
+    elif arg_len >= 1:
+        print("{} arguments:".format(arg_len), end="\n")
+        for i in range(1, arg_len + 1):
+            print("{}: {}".format(i, sys.argv[i]), end="\n")
     else:
-        print('{} arguments'.format(a), end="\n")
+        print("{} arguments.".format(arg_len), end="\n")
