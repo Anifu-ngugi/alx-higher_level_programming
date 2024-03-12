9ee32ac082d6110b1f15d7d59efbc3147092d5aa
Anifu-ngugi <anifungugi@gmaill.com>
2024-03-12 12:46:25 +0000

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process')
+const argum1 = process.argv[2]
+if (argum1 === undefined) {
+console.log('No argument');
+} else {
+console.log (argum1);
+}
+
+
