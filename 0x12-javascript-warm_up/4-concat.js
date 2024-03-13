f55eed3db933baf30fcb843b3826e7d3cf1ee7ec
Anifu-ngugi <anifungugi1@gmail.com>
2024-03-13 05:47:25 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const process = require('process')
-const [, , arg1, arg2] = process.argv
+const process = require('process');
+const [, , arg1, arg2] = process.argv;
 if (arg1 === undefined) {
-console.log('undefined is undefined');
+  console.log('undefined is undefined');
 } else {
-console.log(`${arg1} is ${arg2}`);
+  console.log(`${arg1} is ${arg2}`);
 }
