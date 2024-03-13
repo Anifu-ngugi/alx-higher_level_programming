fbcec15138b0cf6d08a269582eb709910d238ac3
Anifu-ngugi <anifungugi@gmaill.com>
2024-03-13 10:32:41 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const process = require('process')
 const argum = parseInt(process.argv[2])
 if (!argum){
 console.log('Missing number of occurences');
-} else{
-for(let i = 0; i< argum; i++)
-{
-console.log('C is fun');
-}
+} else {
+  for(let i = 0; i < argum; i++) {
+     console.log('C is fun');
+ }
 }
