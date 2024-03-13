1bafed01af14601dcfefafcaa241ca55d2a0b3b8
Anifu-ngugi <anifungugi@gmaill.com>
2024-03-13 09:11:23 +0000

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const process = require('process')
+const argum1 = process.argv [2]
+if (process.argv.length === 2){
+console.log('Not a number');
+}else {
+console.log('My number:',parseInt(argum1));
+}
