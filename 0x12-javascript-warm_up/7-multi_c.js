0f9a2e3f44c704eee12317f4539fd1a811cd685a
Anifu-ngugi <anifungugi1@gmail.com>
2024-03-13 05:55:29 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const argum = parseInt(process.argv[2])
+const argum = parseInt(process.argv[2]);
 if (!argum) {
-console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
   for (let i = 0; i < argum; i++) {
-     console.log('C is fun');
- }
+    console.log('C is fun');
+  }
 }
